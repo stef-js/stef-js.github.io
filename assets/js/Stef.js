7d2958b11138b4e774ea2b7e1afd6bbbd52a116c
Stefanus Prasetyo Nugroho <34230596+stef-js@users.noreply.github.com>
2018-01-21 21:05:03 +0700

    Update Stef.js

--- assets/js/Stef.js
+++ assets/js/Stef.js
@@ -101,7 +101,7 @@ return this.each(function() {
 */
 +function($){
  
-  $.fn.stefcountdown = function(options){
+  $.fn.stefRedirect = function(options){
     var element = this;
     var setting = $.extend({
        url: 'https://www.google.com/',
@@ -134,6 +134,56 @@ _st(document).ready(function(){
   };
  
 }(jQuery);
+
+/*
+jQuery Countdown
+*/
++function($){
+ 
+  $.fn.stefCountdown = function(options){
+    var element = this;
+    var setting = $.extend({
+       date:'20 Oct,2018 00:01:01',
+       message:'sukses',
+       target:'#demo',
+        },options);
+
+    var el = setting.target;
+        // Set the date we're counting down to
+var countDownDate = new Date(setting.date).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    $(el).html(days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ");
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        $(el).html("EXPIRED");
+    }
+}, 1000);
+
+
+
+          };
+ 
+}(jQuery);
+
 
 /*
   jQuery Slideshow Plugin
